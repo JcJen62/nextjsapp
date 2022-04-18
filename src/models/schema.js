b7de07be215084f3043b42e4a824eb6049695146
jcjen62 <jcjensen62@gmail.com>
2022-04-18 15:00:07 -0600

    Owner

--- src/models/schema.js
+++ src/models/schema.js
@@ -109,6 +109,24 @@ export const schema = {
                                     "delete",
                                     "read"
                                 ]
+                            },
+                            {
+                                "provider": "userPools",
+                                "ownerField": "owner",
+                                "allow": "owner",
+                                "identityClaim": "cognito:username",
+                                "operations": [
+                                    "create",
+                                    "update",
+                                    "delete",
+                                    "read"
+                                ]
+                            },
+                            {
+                                "allow": "private",
+                                "operations": [
+                                    "read"
+                                ]
                             }
                         ]
                     }
